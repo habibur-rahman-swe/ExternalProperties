d04d854800f653c19602403ec513089f4ddf3584
Habibur Rahman <habibur0199mhr@gmail.com>
2023-11-14 19:05:44 +0600

    External Properties

---
+++ src/main/java/com/habib/DiDemoApplication.java
@@ -0,0 +1,13 @@
+package com.habib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiDemoApplication.class, args);
+	}
+
+}
